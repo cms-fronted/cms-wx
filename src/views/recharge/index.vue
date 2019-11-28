f0582ad264e74ab2836fc201b7333712557bb16e
unknown <chenlangbin>
2019-11-28 16:17:20 +0800

    pay

--- src/views/recharge/index.vue
+++ src/views/recharge/index.vue
@@ -134,7 +134,7 @@ export default {
           console.log(res);
           //debugger;
           // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-          if (res.err_msg == "get_brand_wcpay_request：ok") {
+          if (res.err_msg == "get_brand_wcpay_request:ok") {
             Toast.success("支付成功！");
             vm.$router.push("index");
           } else {
