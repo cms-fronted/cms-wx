ffaa1d2c8d0b77da2d32cfae2337f92f87067bdb
unknown <chenlangbin>
2019-11-28 12:07:33 +0800

    pay

--- src/views/recharge/index.vue
+++ src/views/recharge/index.vue
@@ -86,6 +86,7 @@ export default {
     },
     //调用微信js api 支付
     onBridgeReady(data) {
+      console.log("data", data);
       var vm = this;
       WeixinJSBridge.invoke(
         "getBrandWCPayRequest",
@@ -94,7 +95,7 @@ export default {
           appId: data.appId, //公众号名称，由商户传入
           timeStamp: data.timeStamp, //时间戳
           nonceStr: data.nonceStr, //随机串
-          package: data.packageValue, //预支付id
+          package: data.package, //预支付id
           signType: data.signType, //微信签名方式
           paySign: data.paySign //微信签名
         },
@@ -103,10 +104,11 @@ export default {
           //debugger;
           // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
           if (res.err_msg == "get_brand_wcpay_request：ok") {
-            alert("支付成功");
+            Toast.success("支付成功！");
             vm.$router.push("index");
           } else {
             //alert("支付失败,请跳转页面"+res.err_msg);
+            Toast.fail("支付失败！");
             vm.$router.push("recharge");
           }
         }
