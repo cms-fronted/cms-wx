9f4bb68e8d379d5ede62b6abb00b75646dbb9431
陈梓栋 <chenzidong@y2t.com>
2020-01-17 16:30:31 +0800

    修复标题栏，可选饭堂bug

--- src/App.vue
+++ src/App.vue
@@ -128,10 +128,10 @@ export default {
     const params = new URLSearchParams(window.location.search.substring(1)); //查询url
     const code = params.get("code"); //获取url中的code
     const state = params.get("state");
-    localStorage.setItem("phone", 1);
-    localStorage.setItem("user_token", "e1541b80101d5fb327b60d7867fffef0");
-    localStorage.setItem("canteen_selected", 1);
-    localStorage.setItem("canteen_id", 130);
+    // localStorage.setItem("phone", 1);
+    // localStorage.setItem("user_token", "e1541b80101d5fb327b60d7867fffef0");
+    // localStorage.setItem("canteen_selected", 1);
+    // localStorage.setItem("canteen_id", 130);
 
     if (!localStorage.getItem("user_token") && !code) {
       this.getCode();
