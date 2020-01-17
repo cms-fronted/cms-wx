ec42f72513f3c6502b8c99af98ad8f846390f14e
陈梓栋 <chenzidong@y2t.com>
2020-01-17 10:38:13 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
 module.exports = {
   publicPath: IS_PROD ? process.env.VUE_APP_PUBLIC_PATH : "./", // 默认'/'，部署应用包时的基本 URL
   outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
-  assetsDir: "", // 相对于outputDir的静态资源(js、css、img、fonts)目录
+  assetsDir: "assets", // 相对于outputDir的静态资源(js、css、img、fonts)目录
   lintOnSave: false,
   runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
   productionSourceMap: !IS_PROD, // 生产环境的 source map
@@ -45,6 +45,6 @@ module.exports = {
     if (IS_PROD) {
       config.optimization.delete('splitChunks')
     }
-    return config
+    return config;
   }
 };
