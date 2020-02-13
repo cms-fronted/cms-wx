5deeac0a0fb2fda9116010d77721ebe64b3e1dc5
unknown <chenlangbin>
2020-02-13 16:02:41 +0800

    注释缓存

--- src/App.vue
+++ src/App.vue
@@ -124,9 +124,9 @@ export default {
     const state = params.get("state");
     // localStorage.setItem("phone", 1);
     // localStorage.setItem("user_token", "10144b391afced8cf534434b34006e79");
-    // localStorage.setItem("user_token", "828e944ae1687fd10f6f62760ba84015");
+    // localStorage.setItem("user_token", "3f77ba51c875238e73a1aa261a1a0b61");
     // localStorage.setItem("canteen_selected", 1);
-    // localStorage.setItem("canteen_id", 130);
+    // localStorage.setItem("canteen_id", 146);
 
     if (!localStorage.getItem("user_token") && !code) {
       this.getCode();
