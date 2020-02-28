d5ccf39a58d4fa9459808136394eb69a28daed96
陈梓栋 <chenzidong@y2t.com>
2020-02-28 23:46:18 +0800

    merge

--- src/api/order.js
+++ src/api/order.js
@@ -26,7 +26,7 @@ export function cancelOrder(data){
 //获取提货码
 export function deliveryCode(data){
 	return request({
-		url:'v1/shop/order/deliveryCode',
+		url:'https://tonglingok.com/canteen/api/v1/shop/order/deliveryCode',
 		method:'get',
 		params:data
 	});
