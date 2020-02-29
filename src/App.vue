882a32654b5e1cbe99c486e9d63f7ae83701a7e8
chenlangbin <chenlangbinn>
2020-02-29 20:53:04 +0800

    新增地址限制功能

--- src/App.vue
+++ src/App.vue
@@ -163,9 +163,9 @@ export default {
     const params = new URLSearchParams(window.location.search.substring(1)); //查询url
     const code = params.get("code"); //获取url中的code
     const state = params.get("state");
-    localStorage.setItem("phone", 1);
-    localStorage.setItem("user_token", "5eb74dc39f2edf65a2af9d3015be88b9");
-    localStorage.setItem("canteen_selected",1);
+    // localStorage.setItem("phone", 1);
+    // localStorage.setItem("user_token", "5eb74dc39f2edf65a2af9d3015be88b9");
+    // localStorage.setItem("canteen_selected",1);
     // localStorage.setItem("canteen_id", 1);
     if (!localStorage.getItem("user_token") && !code) {
       this.getCode();
